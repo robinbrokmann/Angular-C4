96015c4f110f60a37890424a17be4d6467cb8937
robin <106778@student.drenthecollege.nl>
2020-10-29 15:23:29 +0100

    Studieblok 8 Klaar

---
+++ Studieblok 8/Opdracht1/myController.js
@@ -0,0 +1,3 @@
+app.controller('customersCtrl', function($scope, $http) {
+    $scope.hoverColor = "";
+});
---
+++ Studieblok 8/Opdracht2/myController.js
@@ -0,0 +1,6 @@
+app.controller('customersCtrl', function($scope, $http) {
+    $scope.hoverColor = "";
+    $scope.redClick = 0;
+    $scope.blueClick = 0;
+    $scope.greenClick = 0;
+});
