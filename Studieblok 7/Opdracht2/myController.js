ef8955f97c00f74572fc2bdaf310f1bbdc6fbb91
robin <106778@student.drenthecollege.nl>
2020-10-29 10:16:48 +0100

    Studieblok 7 klaar

---
+++ Studieblok 7/Opdracht2/myController.js
@@ -0,0 +1,8 @@
+app.controller('myCtrl', function($scope) {
+    $scope.autoArray = [
+        honda={kenteken: '56-ri-69', kleur: 'Rood', merk: 'Honda', aantaldeuren: 4},
+        subaru={kenteken: 'ab-789-f', kleur: 'Blauw', merk: 'Subaru', aantaldeuren: 4},
+        bmw={kenteken: 'ar-45-ba', kleur: 'Grijs', merk: 'BMW', aantaldeuren: 2},
+        opel={kenteken: 'iz-49-nw', kleur: 'Groen', merk: 'Opel', aantaldeuren: 6},
+        nissan={kenteken: 'ka-985-i', kleur: 'Zwart', merk: 'Nissan', aantaldeuren: 2}];
+});
